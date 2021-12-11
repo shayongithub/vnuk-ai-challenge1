0a7be4f7c57b9de46d5e7aba7e505a97f5253de3
shayongithub <dat.nguyen190401@vnuk.edu.vn>
2021-12-11 21:43:01 +0700

    update CNN model

--- quickdraw_app.py
+++ quickdraw_app.py
@@ -8,21 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # data = 'some data'
-    # list = ["1", "2", "3"]
 
     return render_template('index.html')
 
 @app.route('/recognize', methods = ['POST'])
 def recognize():
-    # theta = np.loadtxt(r'theta_all.txt')
+
     if request.method == 'POST':
         print('Receive image and start predicting')
         data = request.get_json()
         imageBase64 = data['image']
         imgBytes = base64.b64decode(imageBase64)
 
-        with open('temp.jpg', 'wb') as temp:
+        with open('predict_img.jpg', 'wb') as temp:
             temp.write(imgBytes)
 
         time.sleep(1)
@@ -31,16 +29,22 @@ def recognize():
         image = cv2.imread('temp.jpg')
         image = cv2.resize(image, (28,28), interpolation=cv2.INTER_AREA)
         img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img_predict = np.reshape(img_gray, (28,28,1))
+
+        img_predict = (255 - img_gray.reshape(1, 28, 28).astype('float32'))/255.
         print(img_predict.shape)
+        print(img_predict)
 
         # Load trained model
+        classes_name = ['The Eiffel Tower', 'Axe', 'Bicycle', 'Golf Club', 'Pizza']
         model = tf.keras.models.load_model('saved_model/quickdraw.h5')
-        prediction = model.predict_classes(img_predict)
+        print(model.summary())
+        prediction = int(np.argmax(model.predict(img_predict)))
+        print('Model predict: ',model.predict(img_predict))
+        predict_class = classes_name[prediction]
 
-        print('Prediction: ',prediction)
+        print('Prediction: ',predict_class)
         return jsonify({
-                'prediction': str(prediction),
+                'prediction': str(predict_class),
                 'status': True,
         })
 
